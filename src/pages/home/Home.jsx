38d180439ea3a196b8c57e2e67205eab56d05aed
Vidit <viditgoel2k@gmail.com>
2024-01-21 01:44:26 +0530

    corrected import

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
 import { useContext } from "react";
-import Testimonial from "../../components/Testimonial/Testimonial";
+import Testimonial from "../../components/testimonial/Testimonial";
 import Filter from "../../components/filter/Filter";
 import HeroSection from "../../components/heroSection/HeroSection";
 import ProductCard from "../../components/productCard/ProductCard";
 import Track from "../../components/track/Track";
-import Layout from "./../../components/layout/Layout";
+import Layout from "../../components/layout/Layout";
 import myContext from "../../context/myContext";
 // import { useDispatch, useSelector } from "react-redux";
 // import { addToCart, deleteFromCart } from '../../redux/cartSlice'
@@ -34,16 +34,16 @@ const Home = () => {
 
   return (
     <Layout>
-      {/* <div className="flex gap-5 justify-center">
-        <button className=' bg-gray-300 p-5' onClick={()=> addCart()}>add</button>
-        <button className=' bg-gray-300 p-5' onClick={()=> deleteCart()}>delete</button>
+      {/* <div className="flex justify-center gap-5">
+        <button className='p-5 bg-gray-300 ' onClick={()=> addCart()}>add</button>
+        <button className='p-5 bg-gray-300 ' onClick={()=> deleteCart()}>delete</button>
       </div> */}
       <HeroSection />
       <Filter />
       <ProductCard />
-      <div className="flex justify-center -mt-10 mb-4">
+      <div className="flex justify-center mb-4 -mt-10">
         <Link to={'/allproducts'}>
-          <button className=' bg-gray-300 px-5 py-2 rounded-xl'>See more</button>
+          <button className='px-5 py-2 bg-gray-300 rounded-xl'>See more</button>
         </Link>
       </div>
       <Track />
